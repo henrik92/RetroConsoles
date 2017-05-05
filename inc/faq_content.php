71e0f3ef9371e484c2353bddd5e0da774d1623b8
WSHENRIK\Henrik <Henrik.Weeken@maksimo.de>
2017-05-05 19:40:38 +0200

    Support update

--- inc/faq_content.php
+++ inc/faq_content.php
@@ -1,7 +1,7 @@
 <div class="jumbotron bg-sup">
   <h1>Support</h1>
 </div>
-<div class="container-fluid container-padding" style="background-color: #514b4d;">
+<div class="container-fluid container-padding" style="background-color: #282425;">
   <div class="row">
     <div class="col-sm-4 text-center">
       <div class="well">
@@ -48,25 +48,168 @@
 <button class="btn btn-warning" value="refresh">Aktualisieren</button>
 
 </div>
-  <div class="container-fluid container-padding" style="background-color: #282425">
-  <div class="container container-padding" style="background-color: #282425; color:white;">
-      <h2>Dokumentation</h2>
-      <hr>
-      <p>Bitte Konsole auswählen</p>
+  <div class="container-fluid container-padding" style="background-color: #514b4d">
+  <div class="container container-padding" style="background-color: #514b4d; color:white;">
+    <h2>Dokumentation</h2>
+    <hr>
+    <div class="row">
+      <div class="col-sm-12">
+        <div class="tabbable-panel">
+          <div class="tabbable-line">
+            <ul class="nav nav-tabs">
+              <div class="col-sm-4">
+              <li class="active"><a href="#doc" data-toggle="tab">Handbücher</a></li>
+            </div>
+            <div class="col-sm-4">
+              <li><a href="#guide" data-toggle="tab">Guides</a></li>
+            </div>
+            <div class="col-sm-4">
+              <li><a href="#self" data-toggle="tab">Selbstreperatur</a></li>
+            </div>
+            </ul>
+            <div class="tab-content">
+              <div class="tab-pane active" id="doc">
+                <p>Bitte Konsole auswählen</p>
+              </div>
+              <div class="tab-pane" id="guide">
+                <p>Bitte Konsole auswählen</p>
+              </div>
+              <div class="tab-pane" id="self">
+                <p>Bitte Konsole auswählen</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </div>
-<div class="container-fluid container-padding" style="background-color: #514b4d;">
+
+<div class="container-fluid container-padding" style="background-color: #282425;">
+<div class="container container-padding" style="background-color: #282425; color:white;">
+    <h2>Kontakt</h2>
+    <hr>
+    <p>Anschrift</p>
+    <p>Telefon</p>
+    <p>Email</p>
+    <p>Fax</p>
+  </div>
+</div>
+<div class="container-fluid container-padding" style="background-color: #514b4d">
 <div class="container container-padding" style="background-color: #514b4d; color:white;">
-    <h2>Tipps und Tricks zur Selbstreperatur</h2>
+    <h2>Kontaktiere uns direkt!</h2>
     <hr>
-    <p>Bitte Konsole auswählen</p>
+    <!--
+    <?php if(isset($_SESSION['login'])){ ?>
+      <h5>Name: <?php echo $_SESSION['vorname']; echo " "; echo $_SESSION['name']; ?></h5>
+    <?php } else {?>
+      <h5>Name: <input name="name" type="text" placeholder="Name"></input></h5>
+      <?php } ?>-->
+      <div class="container container-padding" style="background-color: #514b4d">
+      <div class="container-fluid">
+
+    <div class="row">
+      <div class="col-sm-6">
+        Vorname:<br>
+        <input type="text"></input>
+      </div>
+      <div class="col-sm-6">
+        Nachname:<br>
+        <input type="text"></input>
+      </div>
+    </div>
+    </div>
+
+    <div class="container-fluid">
+  <div class="row">
+    <div class="col-sm-8">
+      <br><br>
+      Straße:<br>
+      <input type="text text-left"></input>
+      <br><br>
+    </div>
+    <div class="col-sm-4">
+      <br><br>
+      Hausnummer:<br>
+      <input type="text"></input>
+      <br><br>
+    </div>
+  </div>
+  </div>
+
+  <div class="container-fluid">
+<div class="row">
+  <div class="col-sm-4">
+    <br><br>
+    PLZ:<br>
+    <input type="text"></input>
+    <br><br>
+  </div>
+  <div class="col-sm-8">
+    <br><br>
+    Ort:<br>
+    <input type="text"></input>
+    <br><br>
   </div>
 </div>
-<div class="container-fluid container-padding" style="background-color: #8c8c8c">
-<div class="container container-padding" style="background-color: #8c8c8c; color:white;">
-    <h2>Kontaktformular</h2>
-    <hr>
-    <p>Du findest keine Hilfe? Kontaktiere uns direkt!</p>
-    <p>Online Kontaktformular</p>
-  </div>
+</div>
+
+<div class="container-fluid">
+<div class="row">
+<div class="col-sm-4">
+  <br><br>
+  Vorwahl:<br>
+  <input type="text"></input>
+  <br><br>
+</div>
+<div class="col-sm-8">
+  <br><br>
+Telefonnummer:<br>
+<input type="text"></input>
+  <br><br>
+</div>
+</div>
+</div>
+
+<div class="container-fluid">
+<div class="row">
+<div class="col-sm-12">
+  <br><br>
+  E-Mail:<br>
+  <input type="text"></input>
+  <br><br>
+</div>
+</div>
+</div>
+
+<div class="container-fluid">
+<div class="row">
+<div class="col-sm-12">
+  <br><br>
+  Betreff:<br>
+  <select></select>
+  <br><br>
+</div>
+</div>
+</div>
+
+<div class="container-fluid">
+<div class="row">
+<div class="col-sm-12">
+  <br><br>
+  Ihre Nachricht:<br>
+  <textarea></textarea>
+</div>
+</div>
+</div>
+</div>
+<hr>
+<div class="container-fluid">
+<div class="row">
+<div class="col-sm-12">
+  <button class="btn btn-hover">Abschicken</button>
+</div>
+</div>
+</div>
+</div>
 </div>
--- inc/login.php
+++ inc/login.php
@@ -51,7 +51,7 @@ if ($result === false){
 if (mysqli_num_rows($result) > 0) {
   while($row = mysqli_fetch_assoc($result)){
     $_SESSION['session_id'] = session_id();
-    $_SESSION['auth'] = true;
+    $_SESSION['login'] = true;
     $_SESSION['user_id'] = $row['user_id'];
     $_SESSION['vorname'] = $row['vorname'];
     $_SESSION['name'] = $row['name'];
